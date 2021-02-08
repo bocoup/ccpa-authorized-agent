b7be2e34b11dbe209530e95144a33574a0ef5e34
alex.flenniken <alex.flenniken@whiteops.com>
2021-02-08 16:43:04 -0500

    Initial attempt

--- src/fake-api/test.js
+++ src/fake-api/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {Builder} = require('selenium-webdriver');
+const {Builder, By, until} = require('selenium-webdriver');
 const assert = require('assert');
 
 const setupTimeout = 5 * 1000;
@@ -17,15 +17,29 @@ suite('landing page', () => {
       .build();
   });
 
-  setup(() => driver.get(process.env.APP_DOMAIN));
-
   suiteTeardown(async () => {
     driver && await driver.quit();
   });
 
-  test('provides an appropriate title', async () => {
-    const title = await driver.getTitle();
-
-    assert(/california authorized agent/i.test(title));
+  test('completes typical flow', async function () {
+    this.timeout(15000);
+    await driver.get(process.env.APP_DOMAIN);
+    await driver.findElement(By.css('#first-name')).sendKeys('Tessa');
+    await driver.findElement(By.css('#last-name')).sendKeys('Tester');
+    await driver.findElement(By.css('#address-street')).sendKeys('555 Main St.');
+    await driver.findElement(By.css('#address-city')).sendKeys('Santa Monica');
+    await driver.findElement(By.css('#address-zipcode')).sendKeys('90410');
+    await driver.findElement(By.css('#email')).sendKeys('alex@bocoup.com');
+    await driver.findElement(By.css('#phone')).sendKeys('206-643-7362');
+    // await driver.findElement(By.css('#email')).sendKeys('tessa.tester@example.com');
+    // await driver.findElement(By.css('#phone')).sendKeys('555-555-5555');
+    await driver.findElement(By.css('#consent-text')).click();
+    await driver.findElement(By.css('#consent-agent')).click();
+    await driver.findElement(By.css('#consent-response')).click();
+    await driver.findElement(By.css('#consent-email')).click();
+    await driver.findElement(By.css('#consent-policies')).click();
+    await driver.findElement(By.css('#volunteer')).click();
+    await driver.wait(until.elementLocated(By.css('#success')), 5000);
+    assert.strictEqual(true, true);
   });
 });
