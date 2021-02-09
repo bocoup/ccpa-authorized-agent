de7102a8f3a6ae5a0aee659e9d192d9d10c70f40
alex.flenniken <alex.flenniken@whiteops.com>
2021-02-09 11:52:27 -0500

    Minimal viable integration test implemented but not fully functional

--- src/fake-api/index.js
+++ src/fake-api/index.js
@@ -4,14 +4,43 @@ const http = require('http');
 
 const PORT = process.env.PORT;
 
-http.createServer((req, res) => {
-  console.error(req.method, req.url);
+let latestEmail;
+
+const sendEmail = async (req, res) => {
+  const latestEmail = await new Promise(resolve => {
+    let receiving = '';
+    req.on('data', chunk => {
+      receiving += chunk.toString();
+    });
+    req.on('end', () => {
+      resolve(
+        receiving
+          .replace(/&#x2F;/g, '/')
+          .replace(/&amp;/g, '&')
+          .replace(/&#x3D;/g, '=')
+      );
+    });
+  });
+  console.error(req.method, req.url, latestEmail);
 
   res.writeHead(200, {'Content-Type': 'text/json'});
   res.end(JSON.stringify({
     id: '<20151025002517.117282.79817@sandbox-123.mailgun.org>',
     message: 'Queued. Thank you.'
   }));
+};
+
+http.createServer((req, res) => {
+  console.log(req.method, req.path);
+  // if (req.method === 'POST' && req.path === '/v3/fake.org/messages') {
+  // }
+  return sendEmail(req, res);
 }).listen(PORT, () => {
   console.error(`Fake API server listening on port ${PORT}`);
 });
+
+module.exports = { 
+  getLatestEmail: () => latestEmail,
+  verificationSmsSent: () => true,
+  verificationSmsApproved: () => true,
+};
--- src/fake-api/test.js
+++ src/fake-api/test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const {Builder, By, until} = require('selenium-webdriver');
+const {Builder, By} = require('selenium-webdriver');
 const assert = require('assert');
+const { getLatestEmail, verificationSmsSent, verificationSmsApproved } = require('./index');
 
 const setupTimeout = 5 * 1000;
 
-suite('landing page', () => {
+suite('integration', () => {
   let driver;
 
   suiteSetup(async function() {
@@ -21,17 +22,18 @@ suite('landing page', () => {
     driver && await driver.quit();
   });
 
-  test('completes typical flow', async function () {
+  test('completes signup flow', async function () {
     this.timeout(15000);
+
+    // Complete form
     await driver.get(process.env.APP_DOMAIN);
     await driver.findElement(By.css('#first-name')).sendKeys('Tessa');
     await driver.findElement(By.css('#last-name')).sendKeys('Tester');
     await driver.findElement(By.css('#address-street')).sendKeys('555 Main St.');
     await driver.findElement(By.css('#address-city')).sendKeys('Santa Monica');
     await driver.findElement(By.css('#address-zipcode')).sendKeys('90410');
-    await driver.findElement(By.css('#email')).sendKeys('alex@bocoup.com');
+    await driver.findElement(By.css('#email')).sendKeys('tessa.tester@example.com');
     await driver.findElement(By.css('#phone')).sendKeys('206-643-7362');
-    // await driver.findElement(By.css('#email')).sendKeys('tessa.tester@example.com');
     // await driver.findElement(By.css('#phone')).sendKeys('555-555-5555');
     await driver.findElement(By.css('#consent-text')).click();
     await driver.findElement(By.css('#consent-agent')).click();
@@ -39,7 +41,24 @@ suite('landing page', () => {
     await driver.findElement(By.css('#consent-email')).click();
     await driver.findElement(By.css('#consent-policies')).click();
     await driver.findElement(By.css('#volunteer')).click();
-    // await driver.wait(until.elementLocated(By.css('#success')), 5000);
-    assert.strictEqual(true, true);
+    const successText = await driver.findElement(By.css('#success h2')).getText();
+    assert.strictEqual(successText, 'Thanks for signing up! You have a few more steps to enroll');
+    
+    // Click link to verify email
+    let email = getLatestEmail();
+    const emailLink = email.match(/To confirm your email, click <a href="([^"])">/)[1];
+    
+    // Enter code to verify phone
+    await driver.get(emailLink);
+    assert.strictEqual(verificationSmsSent(), true);
+    await driver.findElement(By.css('#code')).sendKeys('99999');
+    await driver.findElement(By.css('#verify-code')).click();
+    
+    // Receive link to authorization form
+    await driver.wait(driver.findElement(By.css('#part-2 h1')).isVisible());
+    assert.strictEqual(verificationSmsApproved(), true);
+    email = getLatestEmail();
+    assert.match(email, /Thank you for enrolling in the Consumer Reports Authorized Agent study/);
+    assert.match(email, /<a href="https:\/\/na4\.docusign\.net\/Member\/PowerFormSigning\.aspx/);
   });
 });
